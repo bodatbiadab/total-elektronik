af6ce0bec9cf3de5a3b51fc228d19da421aa54e4
MR.CEO.BODAT <156589597+bodatbiadab@users.noreply.github.com>
2024-01-18 23:30:56 +0700

    Update show.js

--- total/js/show.js
+++ total/js/show.js
@@ -1,45 +1,62 @@
-const apiUrl = "https://totalelek.totalelek.kencang.id/v1/";
-let capek = "";
-let on = false;
+// Menangkap elemen formulir pencarian
+const apiUrl = "https://totalelek.totalelek.kencang.id/v1/getall";
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const dataContainer = document.getElementById("ac");
+const laptopContainer = document.getElementById("laptop");
+const HAContainer = document.getElementById("home-applience");
+const CBContainer = document.getElementById("cabel");
+const tvContainer = document.getElementById("tv");
+const etcContainer = document.getElementById("etc");
+const mesincuciContainer = document.getElementById("mesincuci");
+const kipasContainer = document.getElementById("kipas");
+const printerContainer = document.getElementById("printer");
+dataContainer.innerHTML = "";
+laptopContainer.innerHTML = "";
+HAContainer.innerHTML = "";
+CBContainer.innerHTML = "";
+tvContainer.innerHTML = "";
+etcContainer.innerHTML = "";
+mesincuciContainer.innerHTML = "";
+kipasContainer.innerHTML = "";
+printerContainer.innerHTML = "";
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Ambil elemen HTML yang akan menampilkan data
-  const dataContainer = document.getElementById("ac");
-  const laptopContainer = document.getElementById("laptop");
-  const HAContainer = document.getElementById("home-applience");
-  const CBContainer = document.getElementById("cabel");
-  const tvContainer = document.getElementById("tv");
-  const etcContainer = document.getElementById("etc");
-  const mesincuciContainer = document.getElementById("mesincuci");
-  dataContainer.innerHTML = "";
-  laptopContainer.innerHTML = "";
-  HAContainer.innerHTML = "";
-  CBContainer.innerHTML = "";
-  tvContainer.innerHTML = "";
-  etcContainer.innerHTML = "";
+// Menambahkan event listener untuk mendengarkan perubahan input
 
-  // URL API yang akan diambil data
+document.addEventListener("DOMContentLoaded", function (event) {
+  event.preventDefault(); // Mencegah formulir dikirim secara default
 
-  // Gunakan fetch untuk melakukan permintaan ke APi
-  fetch(apiUrl + "getall")
-    .then((response) => {
-      // Periksa apakah respons dari API berhasil (status code 200)
-      //   console.log(response.json().then((d) => console.log(d)));
-      function formatRupiah(angka) {
-        var reverse = angka.toString().split("").reverse().join(""),
-          ribuan = reverse.match(/\d{1,3}/g);
-        ribuan = ribuan.join(".").split("").reverse().join("");
-        return "Rp " + ribuan;
-      }
-      // Parse respons sebagai JSON
-      response.json().then((data) => {
-        // dataContainer.innerHTML = "";
-        capek = data;
-        console.log(data);
-        // Iterasi melalui setiap produk dan tambahkan ke elemen HTML
-        data.data.forEach((produk) => {
-          if (produk.kategori === "ac") {
-            dataContainer.innerHTML += `
+  const inputValue = searchBox.value.trim(); // Mendapatkan nilai input dan menghapus spasi di awal dan akhir
+
+  // Lakukan pencarian hanya jika nilai input tidak kosong
+  if (inputValue !== "") {
+    // Lakukan apa pun yang Anda inginkan dengan nilai input, seperti pencarian atau pemfilteran data
+    // Misalnya, kirim nilai input ke server untuk pencarian menggunakan fetch API
+    // fetchSearchResults(inputValue);
+  }
+});
+
+// Fungsi untuk melakukan pencarian menggunakan fetch API (contoh)
+
+fetch(apiUrl)
+  .then((response) => {
+    // Periksa apakah respons dari API berhasil (status code 200)
+    //   console.log(response.json().then((d) => console.log(d)));
+    function formatRupiah(angka) {
+      var reverse = angka.toString().split("").reverse().join(""),
+        ribuan = reverse.match(/\d{1,3}/g);
+      ribuan = ribuan.join(".").split("").reverse().join("");
+      return "Rp " + ribuan;
+    }
+    // Parse respons sebagai JSON
+    response.json().then((data) => {
+      // dataContainer.innerHTML = "";
+      capek = data;
+      console.log(data);
+      // Iterasi melalui setiap produk dan tambahkan ke elemen HTML
+      data.data.forEach((produk) => {
+        if (produk.kategori === "ac") {
+          dataContainer.innerHTML += `
             <div class="menu-card">
             <img src="${
               produk.nama_photo
@@ -48,8 +65,8 @@ document.addEventListener("DOMContentLoaded", function () {
             <p class="menu-card-price">"${formatRupiah(produk.harga)}"</p>
           </div>
         `;
-          } else if (produk.kategori === "laptop-computer") {
-            laptopContainer.innerHTML += `
+        } else if (produk.kategori === "laptop-computer") {
+          laptopContainer.innerHTML += `
             <div class="menu-card">
             <img src="${
               produk.nama_photo
@@ -58,8 +75,8 @@ document.addEventListener("DOMContentLoaded", function () {
             <p class="menu-card-price">"${formatRupiah(produk.harga)}"</p>
           </div>
         `;
-          } else if (produk.kategori === "home appliance") {
-            HAContainer.innerHTML += `
+        } else if (produk.kategori === "home appliance") {
+          HAContainer.innerHTML += `
             <div class="menu-card">
             <img src="${
               produk.nama_photo
@@ -68,8 +85,8 @@ document.addEventListener("DOMContentLoaded", function () {
             <p class="menu-card-price">"${formatRupiah(produk.harga)}"</p>
           </div>
         `;
-          } else if (produk.kategori === "cabel") {
-            CBContainer.innerHTML += `
+        } else if (produk.kategori === "cabel") {
+          CBContainer.innerHTML += `
             <div class="menu-card">
             <img src="${
               produk.nama_photo
@@ -78,8 +95,8 @@ document.addEventListener("DOMContentLoaded", function () {
             <p class="menu-card-price">"${formatRupiah(produk.harga)}"</p>
           </div>
         `;
-          } else if (produk.kategori === "tv") {
-            tvContainer.innerHTML += `
+        } else if (produk.kategori === "tv") {
+          tvContainer.innerHTML += `
             <div class="menu-card">
             <img src="${
               produk.nama_photo
@@ -88,8 +105,8 @@ document.addEventListener("DOMContentLoaded", function () {
             <p class="menu-card-price">"${formatRupiah(produk.harga)}"</p>
           </div>
         `;
-          } else if (produk.kategori === "etc") {
-            etcContainer.innerHTML += `
+        } else if (produk.kategori === "etc") {
+          etcContainer.innerHTML += `
             <div class="menu-card">
             <img src="${
               produk.nama_photo
@@ -98,13 +115,40 @@ document.addEventListener("DOMContentLoaded", function () {
             <p class="menu-card-price">"${formatRupiah(produk.harga)}"</p>
           </div>
         `;
-          }
-        });
+        } else if (produk.kategori === "mesincuci") {
+          mesincuciContainer.innerHTML += `
+            <div class="menu-card">
+            <img src="${
+              produk.nama_photo
+            }" alt="Air Conditioner" class="menu-card-img">
+            <h3 class="menu-card-title">"${produk.nama}"</h3>
+            <p class="menu-card-price">"${formatRupiah(produk.harga)}"</p>
+          </div>
+        `;
+        } else if (produk.kategori === "kipas") {
+          kipasContainer.innerHTML += `
+            <div class="menu-card">
+            <img src="${
+              produk.nama_photo
+            }" alt="Air Conditioner" class="menu-card-img">
+            <h3 class="menu-card-title">"${produk.nama}"</h3>
+            <p class="menu-card-price">"${formatRupiah(produk.harga)}"</p>
+          </div>
+        `;
+        } else if (produk.kategori === "printer") {
+          printerContainer.innerHTML += `
+            <div class="menu-card">
+            <img src="${
+              produk.nama_photo
+            }" alt="Air Conditioner" class="menu-card-img">
+            <h3 class="menu-card-title">"${produk.nama}"</h3>
+            <p class="menu-card-price">"${formatRupiah(produk.harga)}"</p>
+          </div>
+        `;
+        }
       });
-      //   console.log(data);
-    })
-    .catch((error) => {
-      console.error("Fetch error:", error);
-      dataContainer.innerHTML = "Error fetching data from the API";
     });
-});
+    //   console.log(data);
+  })
+
+  .catch((error) => console.error("Error:", error));
